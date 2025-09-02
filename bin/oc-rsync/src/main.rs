516db100e34269243e21d429930e3ea4bcc3b0c0
Ofer Chen <oferchen@gmail.com>
2025-09-02 19:16:46 +0200

    Fix version banner import (#806)

--- bin/oc-rsync/src/main.rs
+++ bin/oc-rsync/src/main.rs
@@ -1,10 +1,9 @@
 // bin/oc-rsync/src/main.rs
 mod version;
 
-use logging::LogFormat;
-use oc_rsync_cli::{cli_command, parse_logging_flags, EngineError};
+use oc_rsync_cli::{cli_command, EngineError};
 use protocol::ExitCode;
-use std::{io::ErrorKind, path::PathBuf};
+use std::io::ErrorKind;
 
 fn exit_code_from_error_kind(kind: clap::error::ErrorKind) -> ExitCode {
     use clap::error::ErrorKind::*;
