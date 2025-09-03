e8283311e5d3bdfd99d9cdf81884f4886a376639
Ofer Chen <oferchen@gmail.com>
2025-09-04 01:15:42 +0200

    add upstream rsync goldens

--- tests/dry_run.rs
+++ tests/dry_run.rs
@@ -2,7 +2,6 @@
 
 use assert_cmd::Command;
 use std::fs;
-use std::process::Command as StdCommand;
 use tempfile::tempdir;
 
 #[test]
@@ -15,16 +14,9 @@ fn dry_run_deletions_match_rsync() {
     fs::write(dst.join("old.txt"), b"old").unwrap();
 
     let src_arg = format!("{}/", src.display());
-    let rsync = StdCommand::new("rsync")
-        .args([
-            "-av",
-            "--delete",
-            "--dry-run",
-            &src_arg,
-            dst.to_str().unwrap(),
-        ])
-        .output()
-        .unwrap();
+    let expected: Vec<_> = include_str!("fixtures/dry_run_deletions.txt")
+        .lines()
+        .collect();
     let ours = Command::cargo_bin("oc-rsync")
         .unwrap()
         .args([
@@ -37,18 +29,12 @@ fn dry_run_deletions_match_rsync() {
         ])
         .output()
         .unwrap();
-    assert_eq!(rsync.status.code(), ours.status.code());
-    let rsync_out = String::from_utf8(rsync.stdout).unwrap();
-    let rsync_lines: Vec<_> = rsync_out
-        .lines()
-        .filter(|l| l.starts_with("deleting "))
-        .collect();
     let our_out = String::from_utf8(ours.stdout).unwrap();
     let our_lines: Vec<_> = our_out
         .lines()
         .filter(|l| l.starts_with("deleting "))
         .collect();
-    assert_eq!(rsync_lines, our_lines);
+    assert_eq!(expected, our_lines);
 }
 
 #[test]
@@ -57,24 +43,19 @@ fn dry_run_errors_match_rsync() {
     let dst = tmp.path().join("dst");
     fs::create_dir_all(&dst).unwrap();
 
-    let rsync = StdCommand::new("rsync")
-        .current_dir(tmp.path())
-        .args(["--dry-run", "missing.txt", dst.to_str().unwrap()])
-        .output()
-        .unwrap();
     let ours = Command::cargo_bin("oc-rsync")
         .unwrap()
         .current_dir(tmp.path())
         .args(["--dry-run", "missing.txt", dst.to_str().unwrap()])
         .output()
         .unwrap();
-    assert_eq!(rsync.status.code(), ours.status.code());
-    let rsync_err = String::from_utf8(rsync.stderr).unwrap();
+    assert_eq!(ours.status.code(), Some(23));
+    let fixture = include_str!("fixtures/dry_run_errors.stderr");
+    let path = tmp.path().join("missing.txt");
+    let expected = fixture.replace("{path}", path.to_str().unwrap());
     let ours_err = String::from_utf8(ours.stderr).unwrap();
-    let mut rs_lines = rsync_err.lines();
+    let mut expected_lines = expected.lines();
     let mut our_lines = ours_err.lines();
-    assert_eq!(rs_lines.next(), our_lines.next());
-    let rs_second = rs_lines.next().unwrap().split(" (code 23)").next().unwrap();
-    let our_second = our_lines.next().unwrap();
-    assert_eq!(format!("{rs_second} (code 23)"), our_second);
+    assert_eq!(expected_lines.next(), our_lines.next());
+    assert_eq!(expected_lines.next(), our_lines.next());
 }
