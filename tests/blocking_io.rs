06805213a1fa8969f4bb4e321dc0d929213ed808
Ofer Chen <oferchen@gmail.com>
2025-09-03 23:17:31 +0200

    Expand version banner

--- tests/blocking_io.rs
+++ tests/blocking_io.rs
@@ -13,8 +13,12 @@ fn version_matches_upstream_nonblocking() {
 
     let up_output = StdCommand::new("rsync").arg("--version").output().unwrap();
     assert!(up_output.status.success());
-
-    assert_eq!(oc_output.stdout, up_output.stdout);
+    let oc_str = String::from_utf8(oc_output.stdout).unwrap();
+    let up_str = String::from_utf8(up_output.stdout).unwrap();
+    let oc_lines: Vec<_> = oc_str.lines().collect();
+    let up_lines: Vec<_> = up_str.lines().collect();
+    let tail = oc_lines.len() - 3;
+    assert_eq!(&oc_lines[3..], &up_lines[1..1 + tail]);
 }
 
 #[test]
@@ -31,6 +35,10 @@ fn version_matches_upstream_blocking() {
         .output()
         .unwrap();
     assert!(up_output.status.success());
-
-    assert_eq!(oc_output.stdout, up_output.stdout);
+    let oc_str = String::from_utf8(oc_output.stdout).unwrap();
+    let up_str = String::from_utf8(up_output.stdout).unwrap();
+    let oc_lines: Vec<_> = oc_str.lines().collect();
+    let up_lines: Vec<_> = up_str.lines().collect();
+    let tail = oc_lines.len() - 3;
+    assert_eq!(&oc_lines[3..], &up_lines[1..1 + tail]);
 }
