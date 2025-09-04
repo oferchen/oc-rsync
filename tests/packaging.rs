bd1cf7a8e7c2a7aed46f25fc72295bde5dde51b8
Ofer Chen <oferchen@gmail.com>
2025-09-04 10:01:53 +0200

    systemd: use single ExecStart (#1079)

--- tests/packaging.rs
+++ tests/packaging.rs
@@ -55,6 +55,10 @@ fn service_unit_matches_spec() {
         "StateDirectory=oc-rsyncd",
         "ConfigurationDirectory=oc-rsyncd",
         "ExecStart=/usr/local/bin/oc-rsyncd --no-detach --config=/etc/oc-rsyncd.conf",
+        "# To run the daemon via the main oc-rsync CLI instead of the dedicated oc-rsyncd",
+        "# binary, create a drop-in with the following lines:",
+        "# ExecStart=",
+        "# ExecStart=/usr/local/bin/oc-rsync --daemon --no-detach --config=/etc/oc-rsyncd.conf",
         "Documentation=man:oc-rsyncd(8) man:oc-rsyncd.conf(5) man:oc-rsync(1)",
     ] {
         assert!(
