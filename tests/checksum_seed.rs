87f33610cbf9d13d3de1f18d95c77d790cad600c
Ofer Chen <oferchen@gmail.com>
2025-09-07 00:16:22 +0200

    test: add default md5 checksum seed test

--- tests/checksum_seed.rs
+++ tests/checksum_seed.rs
@@ -22,28 +22,28 @@ fn checksum_seed_changes_strong_checksum() {
     let strong1 = cfg1.checksum(data).strong;
     let hex0 = hex::encode(&strong0);
     let hex1 = hex::encode(&strong1);
-    assert_eq!(hex0, "7ced6b52c8203ba97580659d7dc33548");
-    assert_eq!(hex1, "681d333539cc115fe7b2f40bb5aa8b89");
+    assert_eq!(hex0, "be4b47980f89d075f8f7e7a9fab84e29");
+    assert_eq!(hex1, "157438ee5881306a9af554cc9b3e5974");
     assert_ne!(hex0, hex1);
 }
 
 #[test]
-fn checksum_seed_changes_strong_checksum_md5() {
+fn checksum_seed_changes_strong_checksum_md4() {
     let data = b"hello world";
     let cfg0 = ChecksumConfigBuilder::new()
         .seed(0)
-        .strong(StrongHash::Md5)
+        .strong(StrongHash::Md4)
         .build();
     let cfg1 = ChecksumConfigBuilder::new()
         .seed(1)
-        .strong(StrongHash::Md5)
+        .strong(StrongHash::Md4)
         .build();
     let strong0 = cfg0.checksum(data).strong;
     let strong1 = cfg1.checksum(data).strong;
     let hex0 = hex::encode(&strong0);
     let hex1 = hex::encode(&strong1);
-    assert_eq!(hex0, "be4b47980f89d075f8f7e7a9fab84e29");
-    assert_eq!(hex1, "157438ee5881306a9af554cc9b3e5974");
+    assert_eq!(hex0, "7ced6b52c8203ba97580659d7dc33548");
+    assert_eq!(hex1, "681d333539cc115fe7b2f40bb5aa8b89");
     assert_ne!(hex0, hex1);
 }
 
