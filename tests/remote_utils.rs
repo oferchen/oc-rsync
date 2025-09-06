e461c4a02e2791257fc4a0ccb9fc450e384f5168
Ofer Chen <oferchen@gmail.com>
2025-09-06 02:48:12 +0200

    Ensure consistent test environment (#1319)

--- tests/remote_utils.rs
+++ tests/remote_utils.rs
@@ -1,11 +1,24 @@
 // tests/remote_utils.rs
 
+use std::process::Command;
 use transport::ssh::SshStdioTransport;
 
 pub fn spawn_reader(cmd: &str) -> SshStdioTransport {
-    SshStdioTransport::spawn("sh", ["-c", cmd]).expect("spawn")
+    let mut c = Command::new("sh");
+    c.args(["-c", cmd])
+        .env("LC_ALL", "C")
+        .env("LANG", "C")
+        .env("COLUMNS", "80")
+        .env("TZ", "UTC");
+    SshStdioTransport::spawn_from_command(c, false).expect("spawn")
 }
 
 pub fn spawn_writer(cmd: &str) -> SshStdioTransport {
-    SshStdioTransport::spawn("sh", ["-c", cmd]).expect("spawn")
+    let mut c = Command::new("sh");
+    c.args(["-c", cmd])
+        .env("LC_ALL", "C")
+        .env("LANG", "C")
+        .env("COLUMNS", "80")
+        .env("TZ", "UTC");
+    SshStdioTransport::spawn_from_command(c, false).expect("spawn")
 }
