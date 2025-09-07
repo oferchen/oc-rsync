5ff3c2a308dcec5f71dbe6b89b74930383906e32
Ofer Chen <oferchen@gmail.com>
2025-09-07 15:15:51 +0200

    chore: remove unsafe env wrappers from tests (#1490)

--- tests/bin_branding.rs
+++ tests/bin_branding.rs
@@ -3,18 +3,10 @@ use assert_cmd::Command;
 use predicates::str::contains;
 use serial_test::serial;
 
-fn set_env(key: &str, value: &str) {
-    unsafe { std::env::set_var(key, value) }
-}
-
-fn remove_env(key: &str) {
-    unsafe { std::env::remove_var(key) }
-}
-
 #[test]
 #[serial]
 fn errors_use_program_name() {
-    set_env("OC_RSYNC_NAME", "myrsync");
+    std::env::set_var("OC_RSYNC_NAME", "myrsync");
     Command::cargo_bin("oc-rsync")
         .unwrap()
         .arg("--bogus")
@@ -22,7 +14,7 @@ fn errors_use_program_name() {
         .failure()
         .stderr(contains("myrsync:"))
         .stderr(contains("myrsync error:"));
-    remove_env("OC_RSYNC_NAME");
+    std::env::remove_var("OC_RSYNC_NAME");
 }
 
 #[test]
--- tests/daemon_syslog.rs
+++ tests/daemon_syslog.rs
@@ -3,18 +3,9 @@
 
 use daemon::init_logging;
 use serial_test::serial;
-use std::ffi::OsStr;
 use std::os::unix::net::UnixDatagram;
 use tempfile::tempdir;
 use tracing::warn;
-
-fn set_env_var<K: AsRef<OsStr>, V: AsRef<OsStr>>(key: K, val: V) {
-    unsafe { std::env::set_var(key, val) }
-}
-
-fn remove_env_var<K: AsRef<OsStr>>(key: K) {
-    unsafe { std::env::remove_var(key) }
-}
 
 #[test]
 #[serial]
@@ -22,7 +13,7 @@ fn daemon_syslog_emits_message() {
     let dir = tempdir().unwrap();
     let path = dir.path().join("sock");
     let server = UnixDatagram::bind(&path).unwrap();
-    set_env_var("OC_RSYNC_SYSLOG_PATH", &path);
+    std::env::set_var("OC_RSYNC_SYSLOG_PATH", &path);
     init_logging(None, None, true, false, false).unwrap();
     warn!(target: "test", "daemon syslog");
     let mut buf = [0u8; 256];
@@ -30,5 +21,5 @@ fn daemon_syslog_emits_message() {
     let msg = std::str::from_utf8(&buf[..n]).unwrap();
     let expected = format!("<12>rsync[{}]: daemon syslog", std::process::id());
     assert_eq!(msg, expected);
-    remove_env_var("OC_RSYNC_SYSLOG_PATH");
+    std::env::remove_var("OC_RSYNC_SYSLOG_PATH");
 }
