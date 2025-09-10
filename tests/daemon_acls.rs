dd2042e8ad59e6de76e04b589ea6486901ec4b72
Ofer Chen <oferchen@gmail.com>
2025-09-10 20:17:38 +0200

    feat: skip missing ACL files in daemon tests (#1719)

--- tests/daemon_acls.rs
+++ tests/daemon_acls.rs
@@ -14,6 +14,48 @@ mod common;
 use common::daemon::spawn_rsync_daemon;
 use common::daemon::{spawn_daemon, wait_for_daemon};
 
+macro_rules! read_acl_or_skip {
+    ($path:expr) => {
+        match PosixACL::read_acl($path) {
+            Ok(acl) => acl,
+            Err(err) => {
+                eprintln!("skipping: {}", err);
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! write_acl_or_skip {
+    ($acl:expr, $path:expr) => {
+        if let Err(err) = $acl.write_acl($path) {
+            eprintln!("skipping: {}", err);
+            return;
+        }
+    };
+}
+
+macro_rules! read_default_acl_or_skip {
+    ($path:expr) => {
+        match PosixACL::read_default_acl($path) {
+            Ok(acl) => acl,
+            Err(err) => {
+                eprintln!("skipping: {}", err);
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! write_default_acl_or_skip {
+    ($acl:expr, $path:expr) => {
+        if let Err(err) = $acl.write_default_acl($path) {
+            eprintln!("skipping: {}", err);
+            return;
+        }
+    };
+}
+
 fn setup_acl_dirs<F>(populate: F) -> (tempfile::TempDir, PathBuf, PathBuf)
 where
     F: FnOnce(&Path),
@@ -28,7 +70,7 @@ where
 }
 
 #[cfg(feature = "root")]
-fn sync_daemon_acls_server() -> (tempfile::TempDir, PathBuf, PathBuf) {
+fn sync_daemon_acls_server() -> Option<(tempfile::TempDir, PathBuf, PathBuf)> {
     let tmp = tempdir().unwrap();
     let src = tmp.path().join("src");
     let srv_oc = tmp.path().join("srv_oc");
@@ -40,14 +82,26 @@ fn sync_daemon_acls_server() -> (tempfile::TempDir, PathBuf, PathBuf) {
     let src_file = src.join("file");
     fs::write(&src_file, b"hi").unwrap();
 
-    let mut acl = PosixACL::read_acl(&src_file).unwrap();
+    let mut acl = match PosixACL::read_acl(&src_file) {
+        Ok(acl) => acl,
+        Err(err) => {
+            eprintln!("skipping: {}", err);
+            return None;
+        }
+    };
     acl.set(Qualifier::User(12345), ACL_READ);
     acl.set(Qualifier::User(23456), ACL_WRITE);
-    acl.write_acl(&src_file).unwrap();
+    if let Err(err) = acl.write_acl(&src_file) {
+        eprintln!("skipping: {}", err);
+        return None;
+    }
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    if let Err(err) = dacl.write_default_acl(&src) {
+        eprintln!("skipping: {}", err);
+        return None;
+    }
 
     let daemon_oc = spawn_daemon(&srv_oc);
     let port_oc = daemon_oc.port;
@@ -67,11 +121,11 @@ fn sync_daemon_acls_server() -> (tempfile::TempDir, PathBuf, PathBuf) {
         .assert()
         .success();
 
-    (tmp, srv_oc, srv_rs)
+    Some((tmp, srv_oc, srv_rs))
 }
 
 #[cfg(feature = "root")]
-fn sync_daemon_acls_client() -> (tempfile::TempDir, PathBuf, PathBuf) {
+fn sync_daemon_acls_client() -> Option<(tempfile::TempDir, PathBuf, PathBuf)> {
     let tmp = tempdir().unwrap();
     let src = tmp.path().join("src");
     let srv_oc = tmp.path().join("srv_oc");
@@ -83,14 +137,26 @@ fn sync_daemon_acls_client() -> (tempfile::TempDir, PathBuf, PathBuf) {
     let src_file = src.join("file");
     fs::write(&src_file, b"hi").unwrap();
 
-    let mut acl = PosixACL::read_acl(&src_file).unwrap();
+    let mut acl = match PosixACL::read_acl(&src_file) {
+        Ok(acl) => acl,
+        Err(err) => {
+            eprintln!("skipping: {}", err);
+            return None;
+        }
+    };
     acl.set(Qualifier::User(12345), ACL_READ);
     acl.set(Qualifier::User(23456), ACL_WRITE);
-    acl.write_acl(&src_file).unwrap();
+    if let Err(err) = acl.write_acl(&src_file) {
+        eprintln!("skipping: {}", err);
+        return None;
+    }
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    if let Err(err) = dacl.write_default_acl(&src) {
+        eprintln!("skipping: {}", err);
+        return None;
+    }
 
     let daemon_oc = spawn_rsync_daemon(&srv_oc, "");
     let port_oc = daemon_oc.port;
@@ -114,7 +180,7 @@ fn sync_daemon_acls_client() -> (tempfile::TempDir, PathBuf, PathBuf) {
         .assert()
         .success();
 
-    (tmp, srv_oc, srv_rs)
+    Some((tmp, srv_oc, srv_rs))
 }
 
 #[test]
@@ -136,10 +202,10 @@ fn daemon_preserves_file_acls() {
     let src_file = src.join("file");
     fs::write(&src_file, b"hi").unwrap();
 
-    let mut acl = PosixACL::read_acl(&src_file).unwrap();
+    let mut acl = read_acl_or_skip!(&src_file);
     acl.set(Qualifier::User(12345), ACL_READ);
     acl.set(Qualifier::User(23456), ACL_WRITE);
-    acl.write_acl(&src_file).unwrap();
+    write_acl_or_skip!(acl, &src_file);
 
     let daemon = spawn_daemon(&srv);
     let port = daemon.port;
@@ -152,8 +218,8 @@ fn daemon_preserves_file_acls() {
         .assert()
         .success();
 
-    let acl_src = PosixACL::read_acl(&src_file).unwrap();
-    let acl_dst = PosixACL::read_acl(srv.join("file")).unwrap();
+    let acl_src = read_acl_or_skip!(&src_file);
+    let acl_dst = read_acl_or_skip!(srv.join("file"));
     assert_eq!(acl_src.entries(), acl_dst.entries());
 }
 
@@ -178,7 +244,7 @@ fn daemon_preserves_default_acls() {
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    write_default_acl_or_skip!(dacl, &src);
 
     let daemon = spawn_daemon(&srv);
     let port = daemon.port;
@@ -191,8 +257,8 @@ fn daemon_preserves_default_acls() {
         .assert()
         .success();
 
-    let dacl_src = PosixACL::read_default_acl(&src).unwrap();
-    let dacl_dst = PosixACL::read_default_acl(&srv).unwrap();
+    let dacl_src = read_default_acl_or_skip!(&src);
+    let dacl_dst = read_default_acl_or_skip!(&srv);
     assert_eq!(dacl_src.entries(), dacl_dst.entries());
 }
 
@@ -211,14 +277,14 @@ fn daemon_preserves_acls_rr_client() {
     let src_file = src.join("file");
     fs::write(&src_file, b"hi").unwrap();
 
-    let mut acl = PosixACL::read_acl(&src_file).unwrap();
+    let mut acl = read_acl_or_skip!(&src_file);
     acl.set(Qualifier::User(12345), ACL_READ);
     acl.set(Qualifier::User(23456), ACL_WRITE);
-    acl.write_acl(&src_file).unwrap();
+    write_acl_or_skip!(acl, &src_file);
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    write_default_acl_or_skip!(dacl, &src);
 
     let daemon = spawn_daemon(&srv);
     let port = daemon.port;
@@ -231,12 +297,12 @@ fn daemon_preserves_acls_rr_client() {
         .assert()
         .success();
 
-    let acl_src = PosixACL::read_acl(&src_file).unwrap();
-    let acl_dst = PosixACL::read_acl(srv.join("file")).unwrap();
+    let acl_src = read_acl_or_skip!(&src_file);
+    let acl_dst = read_acl_or_skip!(srv.join("file"));
     assert_eq!(acl_src.entries(), acl_dst.entries());
 
-    let dacl_src = PosixACL::read_default_acl(&src).unwrap();
-    let dacl_dst = PosixACL::read_default_acl(&srv).unwrap();
+    let dacl_src = read_default_acl_or_skip!(&src);
+    let dacl_dst = read_default_acl_or_skip!(&srv);
     assert_eq!(dacl_src.entries(), dacl_dst.entries());
 }
 
@@ -257,9 +323,9 @@ fn daemon_removes_file_acls() {
     let srv_file = srv.join("file");
     fs::write(&srv_file, b"hi").unwrap();
 
-    let mut acl = PosixACL::read_acl(&srv_file).unwrap();
+    let mut acl = read_acl_or_skip!(&srv_file);
     acl.set(Qualifier::User(12345), ACL_READ);
-    acl.write_acl(&srv_file).unwrap();
+    write_acl_or_skip!(acl, &srv_file);
 
     let daemon = spawn_daemon(&srv);
     let port = daemon.port;
@@ -272,7 +338,7 @@ fn daemon_removes_file_acls() {
         .assert()
         .success();
 
-    let acl_dst = PosixACL::read_acl(&srv_file).unwrap();
+    let acl_dst = read_acl_or_skip!(&srv_file);
     assert!(acl_dst.get(Qualifier::User(12345)).is_none());
 }
 
@@ -293,7 +359,7 @@ fn daemon_removes_default_acls() {
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&srv).unwrap();
+    write_default_acl_or_skip!(dacl, &srv);
 
     let daemon = spawn_daemon(&srv);
     let port = daemon.port;
@@ -306,7 +372,7 @@ fn daemon_removes_default_acls() {
         .assert()
         .success();
 
-    let dacl_dst = PosixACL::read_default_acl(&srv).unwrap();
+    let dacl_dst = read_default_acl_or_skip!(&srv);
     assert!(dacl_dst.entries().is_empty());
 }
 
@@ -320,9 +386,9 @@ fn daemon_ignores_acls_without_flag() {
     let (_tmp, src, srv) = setup_acl_dirs(|src| {
         let src_file = src.join("file");
         fs::write(&src_file, b"hi").unwrap();
-        let mut acl = PosixACL::read_acl(&src_file).unwrap();
+        let mut acl = read_acl_or_skip!(&src_file);
         acl.set(Qualifier::User(12345), ACL_READ);
-        acl.write_acl(&src_file).unwrap();
+        write_acl_or_skip!(acl, &src_file);
     });
 
     let daemon = spawn_daemon(&srv);
@@ -336,7 +402,7 @@ fn daemon_ignores_acls_without_flag() {
         .assert()
         .success();
 
-    let acl_dst = PosixACL::read_acl(srv.join("file")).unwrap();
+    let acl_dst = read_acl_or_skip!(srv.join("file"));
     assert!(acl_dst.get(Qualifier::User(12345)).is_none());
 }
 
@@ -350,7 +416,7 @@ fn daemon_ignores_default_acls_without_flag() {
     let (_tmp, src, srv) = setup_acl_dirs(|src| {
         let mut dacl = PosixACL::new(0o755);
         dacl.set(Qualifier::User(12345), ACL_READ);
-        dacl.write_default_acl(src).unwrap();
+        write_default_acl_or_skip!(dacl, src);
 
         let sub = src.join("sub");
         fs::create_dir(&sub).unwrap();
@@ -369,13 +435,13 @@ fn daemon_ignores_default_acls_without_flag() {
         .assert()
         .success();
 
-    let dacl_dst = PosixACL::read_default_acl(&srv).unwrap();
+    let dacl_dst = read_default_acl_or_skip!(&srv);
     assert!(dacl_dst.get(Qualifier::User(12345)).is_none());
 
-    let dacl_dst_sub = PosixACL::read_default_acl(srv.join("sub")).unwrap();
+    let dacl_dst_sub = read_default_acl_or_skip!(srv.join("sub"));
     assert!(dacl_dst_sub.get(Qualifier::User(12345)).is_none());
 
-    let acl_dst_file = PosixACL::read_acl(srv.join("sub/file")).unwrap();
+    let acl_dst_file = read_acl_or_skip!(srv.join("sub/file"));
     assert!(acl_dst_file.get(Qualifier::User(12345)).is_none());
 }
 
@@ -398,7 +464,7 @@ fn daemon_inherits_directory_default_acls() {
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    write_default_acl_or_skip!(dacl, &src);
 
     let sub = src.join("sub");
     fs::create_dir(&sub).unwrap();
@@ -414,10 +480,10 @@ fn daemon_inherits_directory_default_acls() {
         .assert()
         .success();
 
-    let src_dacl = PosixACL::read_default_acl(&src).unwrap();
-    let src_sub_dacl = PosixACL::read_default_acl(&sub).unwrap();
-    let dst_dacl = PosixACL::read_default_acl(&srv).unwrap();
-    let dst_sub_dacl = PosixACL::read_default_acl(srv.join("sub")).unwrap();
+    let src_dacl = read_default_acl_or_skip!(&src);
+    let src_sub_dacl = read_default_acl_or_skip!(&sub);
+    let dst_dacl = read_default_acl_or_skip!(&srv);
+    let dst_sub_dacl = read_default_acl_or_skip!(srv.join("sub"));
     assert_eq!(
         (src_dacl.entries(), src_sub_dacl.entries()),
         (dst_dacl.entries(), dst_sub_dacl.entries())
@@ -443,7 +509,7 @@ fn daemon_inherits_file_acls_from_default() {
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    write_default_acl_or_skip!(dacl, &src);
 
     let sub = src.join("sub");
     fs::create_dir(&sub).unwrap();
@@ -461,8 +527,8 @@ fn daemon_inherits_file_acls_from_default() {
         .assert()
         .success();
 
-    let acl_src_file = PosixACL::read_acl(&src_file).unwrap();
-    let acl_dst_file = PosixACL::read_acl(srv.join("sub/file")).unwrap();
+    let acl_src_file = read_acl_or_skip!(&src_file);
+    let acl_dst_file = read_acl_or_skip!(srv.join("sub/file"));
     assert_eq!(acl_src_file.entries(), acl_dst_file.entries());
 }
 
@@ -481,7 +547,7 @@ fn daemon_inherits_default_acls_rr_client() {
 
     let mut dacl = PosixACL::new(0o755);
     dacl.set(Qualifier::User(12345), ACL_READ);
-    dacl.write_default_acl(&src).unwrap();
+    write_default_acl_or_skip!(dacl, &src);
 
     let sub = src.join("sub");
     fs::create_dir(&sub).unwrap();
@@ -499,16 +565,16 @@ fn daemon_inherits_default_acls_rr_client() {
         .assert()
         .success();
 
-    let dacl_src = PosixACL::read_default_acl(&src).unwrap();
-    let dacl_dst = PosixACL::read_default_acl(&srv).unwrap();
+    let dacl_src = read_default_acl_or_skip!(&src);
+    let dacl_dst = read_default_acl_or_skip!(&srv);
     assert_eq!(dacl_src.entries(), dacl_dst.entries());
 
-    let dacl_src_sub = PosixACL::read_default_acl(&sub).unwrap();
-    let dacl_dst_sub = PosixACL::read_default_acl(srv.join("sub")).unwrap();
+    let dacl_src_sub = read_default_acl_or_skip!(&sub);
+    let dacl_dst_sub = read_default_acl_or_skip!(srv.join("sub"));
     assert_eq!(dacl_src_sub.entries(), dacl_dst_sub.entries());
 
-    let acl_src_file = PosixACL::read_acl(&src_file).unwrap();
-    let acl_dst_file = PosixACL::read_acl(srv.join("sub/file")).unwrap();
+    let acl_src_file = read_acl_or_skip!(&src_file);
+    let acl_dst_file = read_acl_or_skip!(srv.join("sub/file"));
     assert_eq!(acl_src_file.entries(), acl_dst_file.entries());
 }
 
@@ -524,9 +590,11 @@ fn daemon_file_acls_match_rsync_server() {
         eprintln!("skipping: ACLs unsupported");
         return;
     }
-    let (_tmp, srv_oc, srv_rs) = sync_daemon_acls_server();
-    let acl_oc = PosixACL::read_acl(srv_oc.join("file")).unwrap();
-    let acl_rs = PosixACL::read_acl(srv_rs.join("file")).unwrap();
+    let Some((_tmp, srv_oc, srv_rs)) = sync_daemon_acls_server() else {
+        return;
+    };
+    let acl_oc = read_acl_or_skip!(srv_oc.join("file"));
+    let acl_rs = read_acl_or_skip!(srv_rs.join("file"));
     assert_eq!(acl_oc.entries(), acl_rs.entries());
 }
 
@@ -542,9 +610,11 @@ fn daemon_default_acls_match_rsync_server() {
         eprintln!("skipping: ACLs unsupported");
         return;
     }
-    let (_tmp, srv_oc, srv_rs) = sync_daemon_acls_server();
-    let dacl_oc = PosixACL::read_default_acl(&srv_oc).unwrap();
-    let dacl_rs = PosixACL::read_default_acl(&srv_rs).unwrap();
+    let Some((_tmp, srv_oc, srv_rs)) = sync_daemon_acls_server() else {
+        return;
+    };
+    let dacl_oc = read_default_acl_or_skip!(&srv_oc);
+    let dacl_rs = read_default_acl_or_skip!(&srv_rs);
     assert_eq!(dacl_oc.entries(), dacl_rs.entries());
 }
 
@@ -560,9 +630,11 @@ fn daemon_file_acls_match_rsync_client() {
         eprintln!("skipping: ACLs unsupported");
         return;
     }
-    let (_tmp, srv_oc, srv_rs) = sync_daemon_acls_client();
-    let acl_oc = PosixACL::read_acl(srv_oc.join("file")).unwrap();
-    let acl_rs = PosixACL::read_acl(srv_rs.join("file")).unwrap();
+    let Some((_tmp, srv_oc, srv_rs)) = sync_daemon_acls_client() else {
+        return;
+    };
+    let acl_oc = read_acl_or_skip!(srv_oc.join("file"));
+    let acl_rs = read_acl_or_skip!(srv_rs.join("file"));
     assert_eq!(acl_oc.entries(), acl_rs.entries());
 }
 
@@ -578,8 +650,10 @@ fn daemon_default_acls_match_rsync_client() {
         eprintln!("skipping: ACLs unsupported");
         return;
     }
-    let (_tmp, srv_oc, srv_rs) = sync_daemon_acls_client();
-    let dacl_oc = PosixACL::read_default_acl(&srv_oc).unwrap();
-    let dacl_rs = PosixACL::read_default_acl(&srv_rs).unwrap();
+    let Some((_tmp, srv_oc, srv_rs)) = sync_daemon_acls_client() else {
+        return;
+    };
+    let dacl_oc = read_default_acl_or_skip!(&srv_oc);
+    let dacl_rs = read_default_acl_or_skip!(&srv_rs);
     assert_eq!(dacl_oc.entries(), dacl_rs.entries());
 }
