452103a0ad703cd1fd6ae285f273f55ca4dd5567
Ofer Chen <oferchen@gmail.com>
2025-09-12 20:09:32 +0200

    tests: simplify journald daemon test (#1771)

--- tests/daemon_journald.rs
+++ tests/daemon_journald.rs
@@ -7,7 +7,7 @@ use std::os::unix::net::UnixDatagram;
 use tempfile::tempdir;
 use tracing::warn;
 mod common;
-use common::{temp_env, with_env_var};
+use common::temp_env;
 
 #[test]
 #[serial]
@@ -23,13 +23,4 @@ fn daemon_journald_emits_message() {
     let msg = std::str::from_utf8(&buf[..n]).unwrap();
     let expected = "PRIORITY=4\nSYSLOG_IDENTIFIER=rsync\nMESSAGE=daemon journald\n";
     assert_eq!(msg, expected);
-    with_env_var("OC_RSYNC_JOURNALD_PATH", &path, || {
-        init_logging(None, None, false, true, false).unwrap();
-        warn!(target: "test", "daemon journald");
-        let mut buf = [0u8; 256];
-        let (n, _) = server.recv_from(&mut buf).unwrap();
-        let msg = std::str::from_utf8(&buf[..n]).unwrap();
-        let expected = "PRIORITY=4\nSYSLOG_IDENTIFIER=rsync\nMESSAGE=daemon journald\n";
-        assert_eq!(msg, expected);
-    });
 }
